94a7a154b0809ca50c14666d3b7d55f7e5b113a8
tasiorsky <bart.kaczor@gmail.com>
2024-09-23 22:54:28 +0200

    Track input changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Results from "./components/Results"
 import UserInput from "./components/UserInput"
+import { calculateInvestmentResults } from "./util/investment"
+
+const INITIAL_INPUT = {
+  initialInvestment: 10000,
+  annualInvestment: 5000,
+  expectedReturn: 8,
+  duration: 10  
+}
 
 function App() {
+  const [input, setInput] = useState({ ...INITIAL_INPUT })
+  const [result, setResult] = useState([])
+
+
+  function onInputChanged() {
+    let newResult = calculateInvestmentResults(input)
+    setResult(newResult)
+  }
+
   return <>
     <Header />
-    <UserInput />
+    <UserInput initialInput={INITIAL_INPUT} inputChanged={onInputChanged}/>
     <Results />
   </>
 }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,4 +1,13 @@
-export default function UserInput({ initialValueChanged, annualInvestmentChanged, returnChanged, durationChanged }) {
+export default function UserInput({ initialInput, inputChanged }) {
+    let input = initialInput
+    function updateInput(event) {
+        const { name, value } = event.target
+        input[name] = value
+
+        console.log(name,value)
+        inputChanged(input)
+    }
+
     return <>
         <div id="user-input">
             <div className="input-group">
@@ -6,16 +15,16 @@ export default function UserInput({ initialValueChanged, annualInvestmentChanged
                 <label>Annual investment</label>
             </div>
             <div className="input-group">
-                <input type="number" onChange={initialValueChanged}/>
-                <input type="number" onChange={annualInvestmentChanged}/>
+                <input name="initialInvestment" type="number" value={input.initialInvestment} onChange={updateInput}/>
+                <input name="annualInvestment" type="number" value={input.annualInvestment} onChange={updateInput}/>
             </div>
             <div className="input-group">
                 <label>Expected return</label>
                 <label>Duration</label>
             </div>
             <div className="input-group">
-                <input type="number" onChange={returnChanged}/>
-                <input type="number" onChange={durationChanged}/>
+                <input name="expectedReturn" type="number" value={input.expectedReturn} onChange={updateInput}/>
+                <input name="duration" type="number" value={input.duration} onChange={updateInput}/>
             </div>
         </div>
     </>
